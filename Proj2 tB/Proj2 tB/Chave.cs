fd623e870f8c88357da1ec8ea6f5986e102dc322
luan oliveira <luanoliveira1107@gmail.com>
2022-11-15 23:56:11 -0300

    ajustado projeto 2

---
+++ Proj2 tB/Proj2 tB/Chave.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proj2_tB
+{
+    class Chave
+    {
+        //ATRIBUTOS
+        private int[] numeros;
+        private int[] estrelas;
+
+        //CONSTRUTORES
+        public Chave()
+        {
+            numeros = new int[5];
+            estrelas = new int[2];
+        }
+
+        //METODOS
+        public void SetNumeros(int[] nNumeros)
+        {
+            numeros = nNumeros;
+        }
+        public int[] GetNumeros()
+        {
+            return numeros;
+        }
+        public void SetEstrelas(int[] nEstrelas)
+        {
+            estrelas = nEstrelas;
+        }
+        public int[] GetEstrelas()
+        {
+            return estrelas;
+        }
+
+
+        public override string ToString()
+        {
+            string chave = "";
+
+            for (int i = 0; i < numeros.Length; i++)
+            {
+                chave += numeros[i] + " ";
+            }
+            chave += "+ ";
+            for (int i = 0; i < estrelas.Length; i++)
+            {
+                chave += estrelas[i] + " ";
+            }
+
+            return chave;
+        }
+    }
+}
+
+//Criar metodo para adicionar numero e estrela uma a uma, fazendo verificações
---
+++ Proj2 tB/Proj2 tB/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proj2_tB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonVP_Click(object sender, EventArgs e)
+        {
+            int[] numeros = new int[5];
+            int[] estrelas = new int[2];
+
+            numeros[0] = int.Parse(comboBox1.Text);
+            numeros[1] = int.Parse(comboBox2.Text);
+            numeros[2] = int.Parse(comboBox3.Text);
+            numeros[3] = int.Parse(comboBox4.Text);
+            numeros[4] = int.Parse(comboBox5.Text);
+
+            estrelas[0] = int.Parse(comboBoxE1.Text);
+            estrelas[1] = int.Parse(comboBoxE2.Text);
+
+            Chave c1 = new Chave();
+
+            c1.SetNumeros(numeros);
+            c1.SetEstrelas(estrelas);
+
+            MessageBox.Show(c1.ToString());
+        }
+    }
+}
